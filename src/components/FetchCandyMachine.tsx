ce220ebbc9c04632c26e0c1eed5858eb7d19a7c6
Francisco <fwmcomputer@gmail.com>
2023-01-08 15:53:43 +0100

    Display wallet nd candy machine nfts

--- src/components/FetchCandyMachine.tsx
+++ src/components/FetchCandyMachine.tsx
@@ -5,18 +5,76 @@ import { FC, useEffect, useState } from "react"
 import styles from "../styles/custom.module.css"
 
 export const FetchCandyMachine: FC = () => {
-  const [candyMachineAddress, setCandyMachineAddress] = useState(null)
+  const [candyMachineAddress, setCandyMachineAddress] = useState("9o6QmfJTd3mPkbJ3vRJ1tkggERwyJ2sDDX4bnJk1yq4A")
   const [candyMachineData, setCandyMachineData] = useState(null)
   const [pageItems, setPageItems] = useState(null)
   const [page, setPage] = useState(1)
+  const { connection } = useConnection()
+  const metaplex = Metaplex.make(connection)
 
-  const fetchCandyMachine = async () => {}
+  // fetch candymachine by address
+  const fetchCandyMachine = async () => {
+      
+    // Set page to 1 - we wanna be at the first page whenever we fetch a new Candy Machine
+    setPage(1)
 
-  const getPage = async (page, perPage) => {}
+    // fetch candymachine data
+    try {
+      const candyMachine = await metaplex
+        .candyMachinesV2()
+        .findByAddress({ address: new PublicKey(candyMachineAddress) })
 
-  const prev = async () => {}
+      setCandyMachineData(candyMachine)
+    } catch (e) {
+      alert("Please submit a valid CMv2 address.")
+    }
+  }
 
-  const next = async () => {}
+  // paging
+  const getPage = async (page, perPage) => {
+    const pageItems = candyMachineData.items.slice(
+      (page - 1) * perPage,
+      page * perPage
+    )
+
+    // fetch metadata of NFTs for page
+    let nftData = []
+    for (let i = 0; i < pageItems.length; i++) {
+      let fetchResult = await fetch(pageItems[i].uri)
+      let json = await fetchResult.json()
+      nftData.push(json)
+    }
+
+    // set state
+    setPageItems(nftData)
+  }
+
+  // previous page
+  const prev = async () => {
+    if (page - 1 < 1) {
+      setPage(1)
+    } else {
+      setPage(page - 1)
+    }
+  }
+
+  // next page
+  const next = async () => {
+    setPage(page + 1)
+  }
+
+  // fetch placeholder candy machine on load
+  useEffect(() => {
+    fetchCandyMachine()
+  }, [])
+
+  // fetch metadata for NFTs when page or candy machine changes
+  useEffect(() => {
+    if (!candyMachineData) {
+      return
+    }
+    getPage(page, 9)
+  }, [candyMachineData, page])
 
   return (
     <div>
@@ -24,6 +82,7 @@ export const FetchCandyMachine: FC = () => {
         type="text"
         className="form-control block mb-2 w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none text-center"
         placeholder="Enter Candy Machine v2 Address"
+        value={candyMachineAddress}
         onChange={(e) => setCandyMachineAddress(e.target.value)}
       />
       <button
